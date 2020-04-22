62a962f57c4448e8ed7d58340759cc449ee95180
vsuzdaltsev <suzdaltsev_v@mail.ru>
2020-04-22 15:23:09 +0300

    rubo

--- src/lib/rest_api.rb
+++ src/lib/rest_api.rb
@@ -15,7 +15,7 @@ class RestApi < Sinatra::Base
 
   VALID_HTTP_METHODS = TestAppConf::DEFAULTS[:valid_http_methods]
   VERSION            = TestAppConf::DEFAULTS[:version].call
-  POSTGRES_HOST      = 'postgres' #-postgresql.default.svc.cluster.local'
+  POSTGRES_HOST      = 'postgres-postgresql.default.svc.cluster.local'
   POSTGRES_DB        = 'yaa'
   POSTGRES_PASSWORD  = 'yaa'
   POSTGRES_USER      = 'postgres'
@@ -39,9 +39,6 @@ class RestApi < Sinatra::Base
     end
   end
 
-
-
-
   def self.seasons_form
     get '/seasons_form' do
       erb :seasons_form
@@ -53,7 +50,7 @@ class RestApi < Sinatra::Base
       puts "My name is #{params[:name]}, and I love #{params[:favorite_season]}"
 
       con = PG.connect(dbname: POSTGRES_DB, user: POSTGRES_USER, host: POSTGRES_HOST, password: POSTGRES_PASSWORD)
-          con.exec("CREATE TABLE if not exists #{TABLE} (name VARCHAR(100) NOT NULL, season VARCHAR(100) NULL);")
+      con.exec("CREATE TABLE if not exists #{TABLE} (name VARCHAR(100) NOT NULL, season VARCHAR(100) NULL);")
       con.exec("INSERT INTO seasons(name, season) VALUES(\'#{params[:name]}\', \'#{params[:favorite_season]}\');")
       # con.close if con
 
